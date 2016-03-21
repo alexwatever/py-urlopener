f614e8bce4aac1cf96f7ae685bfa76a66e4dd5c9
Alex Watson <dev.iamlolz@gmail.com>
2016-03-20 21:31:13 -0400

    setup basic template structure

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return "Hello world!"
+    user = {'nickname': 'Alex'} # fake user
+    return render_template('index.html',
+        title='Home',
+        user=user)
 
