afbebac8398a4a84df85a252daacb71ec590202b
Alex Watson <dev.iamlolz@gmail.com>
2016-04-10 23:57:57 -0400

    design changes to tidy up page

--- app/static/js/global.js
+++ app/static/js/global.js
@@ -1,45 +1,68 @@
-// init
+// materializecss init
 $(document).ready(function(){
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
 });
 
 
-// main url opener function
+// prevent form natively firing and instead store input data
 $(document).ready(function() {
   $('#formURLs').on('submit', function(e) {
     // prevent form from submitting normally and refreshing page
     e.preventDefault();
 
-    // store textarea field into variable
-    var lines = $('#textareaURLs').val().split(/\n/);
+    // store data from form into variable
+    var data = $('#textareaURLs').val();
 
-    // create empty variables
-    var texts = []; // for array of urls
-    var win = ''; // for opening urls in tabs
-
-    // loop through list of urls to validate
-    for (var i=0; i < lines.length; i++) {
-      // only manage this line if it contains a non whitespace character.
-      if (/\S/.test(lines[i])) {
-
-        // validate url and add http protocol if missing
-        if (!lines[i].match(/^[a-zA-Z]+:\/\//))
-        {
-          lines[i] = 'http://' + lines[i];
-        }
-
-        // push line to array
-        texts.push($.trim(lines[i]));
-      };
-    };
-
-    // get length of array
-    var textsLength = texts.length;
-
-    // loop through array of urls and open
-    for (var i=0; i < textsLength; i++) {
-      win = window.open(texts[i], '_blank');
-    };
+    // validate data
+    validateURLs(data);
   });
 });
+
+
+// validate data
+function validateURLs(data) {
+  // split data lines into array
+  var lines = data.split(/\n/);
+
+  // create empty array to store valid urls
+  var urlArray = [];
+
+  // loop through list of urls to validate
+  for (var i=0; i < lines.length; i++) {
+    // handle array item if it contains a non whitespace character.
+    if (/\S/.test(lines[i])) {
+      // add http protocol if missing
+      if (!lines[i].match(/^[a-zA-Z]+:\/\//))
+      {
+        lines[i] = 'http://' + lines[i];
+      };
+
+      // push line to array
+      urlArray.push($.trim(lines[i]));
+    } else {
+      // alert if no valid text is entered
+      alert('Please enter at least one valid url.');
+    };
+  };
+
+  // store length of array
+  var arrayLength = urlArray.length;
+
+  // if items exist in array then open urls
+  if (arrayLength > 0) {
+    openURLs(urlArray, arrayLength);
+  };
+};
+
+
+// open validated list of urls
+function openURLs(urlArray, arrayLength) {
+  // variable for opening urls in tabs
+  var win = '';
+
+  // loop through array of urls and open
+  for (var i=0; i < arrayLength; i++) {
+    win = window.open(urlArray[i], '_blank');
+  };
+};
